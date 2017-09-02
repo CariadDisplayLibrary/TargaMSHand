ed2083b81d6bb0d5ecfe4526095787e7e6357319
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:36:33 +0100

    Initial import

---
+++ TargaMSHand.h
@@ -0,0 +1,29 @@
+#ifndef _TARGAMSHAND_H
+#define _TARGAMSHAND_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t TargaMSHand10[];
+    extern const uint8_t TargaMSHand11[];
+    extern const uint8_t TargaMSHand12[];
+    extern const uint8_t TargaMSHand13[];
+    extern const uint8_t TargaMSHand14[];
+    extern const uint8_t TargaMSHand15[];
+    extern const uint8_t TargaMSHand16[];
+    extern const uint8_t TargaMSHand17[];
+    extern const uint8_t TargaMSHand18[];
+    extern const uint8_t TargaMSHand19[];
+    extern const uint8_t TargaMSHand20[];
+    extern const uint8_t TargaMSHand21[];
+    extern const uint8_t TargaMSHand22[];
+    extern const uint8_t TargaMSHand23[];
+    extern const uint8_t TargaMSHand24[];
+    extern const uint8_t TargaMSHand25[];
+    extern const uint8_t TargaMSHand26[];
+    extern const uint8_t TargaMSHand27[];
+    extern const uint8_t TargaMSHand28[];
+    extern const uint8_t TargaMSHand29[];
+    extern const uint8_t TargaMSHand30[];
+};
+#endif
